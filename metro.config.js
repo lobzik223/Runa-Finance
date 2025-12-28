f4b2f227603f96e0869e9bf4fd150b99908f791a
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 14:44:27 +0300

    Fix Metro config to transpile react-native for phone builds

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,24 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+// Включаем транспиляцию для react-native из node_modules
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+  unstable_allowRequireContext: true,
+};
 
+// Настраиваем resolver для транспиляции react-native
+config.resolver = {
+  ...config.resolver,
+  blockList: [],
+  unstable_enablePackageExports: true,
+};
+
+module.exports = config;
