ff0ae083b454375b3b502937352934a819a704c1
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 13:18:55 +0300

    babelconfig f3x

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,29 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
+      [
+        '@babel/plugin-transform-class-properties',
+        {
+          loose: true,
+        },
+      ],
+      [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true,
+        },
+      ],
       'react-native-reanimated/plugin',
     ],
+    env: {
+      production: {
+        plugins: [
+          '@babel/plugin-transform-class-properties',
+          '@babel/plugin-transform-private-methods',
+          'react-native-reanimated/plugin',
+        ],
+      },
+    },
   };
 };
 
