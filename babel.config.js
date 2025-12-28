ebb9af3f9427c1dac78b39f2b5e35531ec11b819
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 14:24:58 +0300

    Add custom transformer to transpile react-native from node_modules

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,38 @@
 module.exports = function(api) {
   api.cache(true);
+  
+  // Применяем плагины ко всем файлам, включая node_modules/react-native
+  const plugins = [
+    [
+      '@babel/plugin-transform-class-properties',
+      { loose: true },
+    ],
+    [
+      '@babel/plugin-transform-private-methods',
+      { loose: true },
+    ],
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      { loose: true },
+    ],
+    // react-native-reanimated должен быть последним
+    'react-native-reanimated/plugin',
+  ];
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        '@babel/plugin-transform-class-properties',
-        { loose: true },
-      ],
-      [
-        '@babel/plugin-transform-private-methods',
-        { loose: true },
-      ],
-      [
-        '@babel/plugin-transform-private-property-in-object',
-        { loose: true },
-      ],
-      // react-native-reanimated должен быть последним
-      'react-native-reanimated/plugin',
-    ],
-    // Применяем плагины ко всем файлам, включая node_modules/react-native
-    env: {
-      production: {
+    plugins: plugins,
+    // Применяем плагины ко всем файлам
+    overrides: [
+      {
+        // Применяем плагины к react-native
+        test: /node_modules[\\/]react-native[\\/]/,
         plugins: [
           '@babel/plugin-transform-class-properties',
           '@babel/plugin-transform-private-methods',
           '@babel/plugin-transform-private-property-in-object',
-          'react-native-reanimated/plugin',
         ],
       },
-    },
+    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,53 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Включаем транспиляцию для react-native из node_modules
+// Кастомный transformer для транспиляции react-native
+const originalTransformer = config.transformer;
+
 config.transformer = {
-  ...config.transformer,
+  ...originalTransformer,
   getTransformOptions: async () => ({
     transform: {
       experimentalImportSupport: false,
       inlineRequires: true,
     },
   }),
-  // Явно указываем, что нужно транспилировать react-native
+  // Используем кастомный transformer
   babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
   // Включаем транспиляцию для всех модулей
   unstable_allowRequireContext: true,
 };
 
-// Настраиваем resolver - разрешаем транспиляцию react-native
+// Настраиваем resolver - явно указываем транспиляцию react-native
 config.resolver = {
   ...config.resolver,
   // Не блокируем react-native
   blockList: [],
   // Включаем транспиляцию для react-native
   unstable_enablePackageExports: true,
+  // Явно указываем, что react-native должен транспилироваться
+  sourceExts: [...config.resolver.sourceExts],
 };
+
+// Настраиваем serializer
+config.serializer = {
+  ...config.serializer,
+  getModulesRunBeforeMainModule: () => [],
+};
+
+// Переопределяем функцию shouldTransform для транспиляции react-native
+const originalShouldTransform = config.transformer.shouldTransform;
+if (originalShouldTransform) {
+  config.transformer.shouldTransform = (filePath) => {
+    // Транспилируем react-native из node_modules
+    if (filePath.includes('node_modules/react-native')) {
+      return true;
+    }
+    return originalShouldTransform(filePath);
+  };
+}
 
 module.exports = config;
