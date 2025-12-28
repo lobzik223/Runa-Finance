04f260f419f5991bf992e41a1143b77778b6ed6f
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 13:54:20 +0300

    Fix Metro config to transpile react-native from node_modules

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,18 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-      '@babel/plugin-transform-private-property-in-object',
+      [
+        '@babel/plugin-transform-class-properties',
+        { loose: true }
+      ],
+      [
+        '@babel/plugin-transform-private-methods',
+        { loose: true }
+      ],
+      [
+        '@babel/plugin-transform-private-property-in-object',
+        { loose: true }
+      ],
       'react-native-reanimated/plugin',
     ],
   };
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,26 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Включаем транспиляцию для react-native
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+  // Включаем транспиляцию для всех модулей
+  unstable_allowRequireContext: true,
+};
+
+// Настраиваем resolver - разрешаем транспиляцию react-native
+config.resolver = {
+  ...config.resolver,
+  // Не блокируем react-native
+  blockList: [],
+  // Включаем транспиляцию для react-native
+  unstable_enablePackageExports: true,
+};
+
 module.exports = config;
