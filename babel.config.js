05df9793c61815af91290b862ab5d2e0aac27c98
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 13:55:24 +0300

    Update Babel config with explicit preset options

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,31 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Явно включаем поддержку приватных методов
+          native: {
+            unstable_transformProfile: 'default',
+          },
+        },
+      ],
+    ],
     plugins: [
       [
         '@babel/plugin-transform-class-properties',
-        { loose: true }
+        { loose: true },
       ],
       [
         '@babel/plugin-transform-private-methods',
-        { loose: true }
+        { loose: true },
       ],
       [
         '@babel/plugin-transform-private-property-in-object',
-        { loose: true }
+        { loose: true },
       ],
+      // react-native-reanimated должен быть последним
       'react-native-reanimated/plugin',
     ],
   };
