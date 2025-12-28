cb18d9079f8b5e4557910ed018194099fb333a5a
Maksim <fazzzy1234ad@gmail.com>
2025-12-28 15:02:26 +0300

    Add Metro config to transpile react-native and Babel overrides for node_modules

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,19 @@ module.exports = function(api) {
         '@babel/plugin-transform-private-property-in-object',
         { loose: true },
       ],
+      // react-native-reanimated должен быть последним
       'react-native-reanimated/plugin',
+    ],
+    // Применяем плагины ко всем файлам, включая node_modules/react-native
+    overrides: [
+      {
+        test: /node_modules[\\/]react-native[\\/]/,
+        plugins: [
+          '@babel/plugin-transform-class-properties',
+          '@babel/plugin-transform-private-methods',
+          '@babel/plugin-transform-private-property-in-object',
+        ],
+      },
     ],
   };
 };
